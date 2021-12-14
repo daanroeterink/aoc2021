cad756a81fb0630bd0f95b79580aadfa69c5e4e0
Daan Roeterink <daan.roeterink@gmail.com>
2021-12-14 12:50:24 +0100

    Part 2 of day 3 added

--- Day3/day3.py
+++ Day3/day3.py
@@ -6,7 +6,35 @@ lines = f.readlines()
 
 gamma = ""
 
-for i in range(len(lines[0]) - 1):
+# for i in range(len(lines[0]) - 1):
+#     count_0 = 0
+#     count_1 = 0
+#     for line in lines:
+#         if int(line[i]) == 1:
+#             count_1 += 1
+#         else:
+#             count_0 += 1
+#     if count_1 > count_0:
+#         gamma += "1"
+#     else:
+#         gamma += "0"
+
+# print(gamma)
+# print(int(gamma, 2))
+
+# epsilon = gamma.replace('1','2').replace('0','1').replace('2','0')
+
+# print(epsilon)
+# print(int(epsilon, 2))
+
+# print(int(gamma, 2) * int(epsilon, 2))
+
+# Part 2 comes tommorow
+found = False
+i = 0
+while(not found):
+
+    filterchar = ""
     count_0 = 0
     count_1 = 0
     for line in lines:
@@ -14,22 +42,28 @@ for i in range(len(lines[0]) - 1):
             count_1 += 1
         else:
             count_0 += 1
-    if count_1 > count_0:
-        gamma += "1"
+    if count_1 < count_0: # >= for first value < for second value
+        filterchar = "1"
     else:
-        gamma += "0"
+        filterchar = "0"
 
-print(gamma)
-print(int(gamma, 2))
+    filtered = filter(lambda line: line[i] == filterchar, lines)
+    lines = list(filtered)
+    i += 1
+    if len(lines) == 1:
+        found = True
 
-epsilon = gamma.replace('1','2').replace('0','1').replace('2','0')
+print(int(lines[0],2))
 
-print(epsilon)
-print(int(epsilon, 2))
 
-print(int(gamma, 2) * int(epsilon, 2))
 
-# Part 2 comes tommorow
+
+
+
+
+
+
+
 
 
 f.close()
